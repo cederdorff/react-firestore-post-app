d76938539e374a6d6aba9170105747da4155a06a
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-07-17 23:32:41 +0200

    Update SignInPage.js

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -35,7 +35,11 @@ export default function SignInPage({ showLoader }) {
             <h1>Sign In</h1>
             <form onSubmit={signIn}>
                 <input type="email" name="mail" placeholder="Type your mail" />
-                <input type="password" name="password" placeholder="Type your password" />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Type your password"
+                />
                 <p className="text-error">{errorMessage}</p>
                 <button>Sign in</button>
             </form>
